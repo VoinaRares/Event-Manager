050db2701e089f241dd322757264cd6d83c663fd
VoinaRares <voinarares14@gmail.com>
2025-11-19 17:30:37 +0200

    Went back to normal structure

--- src/main/java/com/example/event_manager/repository/AuditLogRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.event_manager.repository;
-import com.example.event_manager.model.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {}
